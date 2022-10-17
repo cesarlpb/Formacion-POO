b22e26fd030f99d25566a20c912839b40147593c
César <cesarlpb@gmail.com>
2022-10-17 16:05:57 +0200

    Plantilla
    index.html, style.css y script.js

---
+++ 1-Modulo/2-IntroWeb-pt2-(17-21Oct)/17-oct-tarde/script.js
@@ -0,0 +1,12 @@
+// Script plantilla
+
+// JS
+// Sacar un dato por consola
+// En navegador: F12 o click derecho Inspeccionar -> Consola
+console.log("Hola desde consola");
+console.log(1+1)
+console.log("César")
+// 
+console.log("Hola desde script.js")
+
+/**********************************/
