887080bc8e01ea120b93156d12274eadc52c0ee5
César <cesarlpb@gmail.com>
2022-10-17 16:56:17 +0200

    Ejemplo JS
    Definición de variables numéricas y string. Operaciones básicas + - * /

--- 1-Modulo/2-IntroWeb-pt2-(17-21Oct)/17-oct-tarde/script.js
+++ 1-Modulo/2-IntroWeb-pt2-(17-21Oct)/17-oct-tarde/script.js
@@ -3,10 +3,42 @@
 // JS
 // Sacar un dato por consola
 // En navegador: F12 o click derecho Inspeccionar -> Consola
-console.log("Hola desde consola");
-console.log(1+1)
-console.log("César")
+// console.log("Hola desde consola");
+// console.log(1+1)
+// console.log("César")
 // 
+// Escribimos mensaje en consola
 console.log("Hola desde script.js")
+/**********************************/
+
+// Defino una variable llamada "nombre" de tipo string (texto)
+var nombre = "Pepe"
+console.log(nombre)
+
+// Operaciones básicas -> + - * /
+// Definimos variables numéricas
+var x = 10
+var y = 2
+
+// Operaciones con mensajes con salto de línea:
+console.log("Operaciones con mensajes con salto de línea:")
+console.log("Suma:")
+console.log(x + y)
+console.log("Resta:")
+console.log(x - y)
+console.log("Multiplicación:")
+console.log(x * y)
+console.log("División:")
+console.log(x / y)
+
+// Operaciones con mensajes en misma línea:
+console.log("Operaciones con mensajes en misma línea:")
+console.log("Suma: " + (x + y))
+console.log("Resta: " + (x - y))
+console.log("Producto: " + (x * y))
+console.log("División: " + (x / y))
+// Hemos usado () para darle prioridad a las operaciones
+// y que salga el resultado correcto
 
 /**********************************/
+console.log("Final del script")
