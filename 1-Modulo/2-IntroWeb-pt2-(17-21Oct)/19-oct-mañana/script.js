7b845387db235c998dd1d55a4b4aa1d2347bcadc
César <cesarlpb@gmail.com>
2022-10-19 08:57:09 +0200

    Plantilla HTML y JS

---
+++ 1-Modulo/2-IntroWeb-pt2-(17-21Oct)/19-oct-mañana/script.js
@@ -0,0 +1,12 @@
+// JS
+// Escribimos mensaje en consola
+console.log("Hola desde script.js")
+/**********************************/
+
+// Encontramos elemento con id "p1" y cambiamos el texto
+document.getElementById("p1").innerText = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos sint esse, culpa illum quasi officia quas magni molestias aspernatur porro."
+// Encontramos elemento con id "p2" y cambiamos el HTML interno
+document.getElementById("p2").innerHTML = "<strong>Texto en negritas: </strong> Más texto."
+
+/**********************************/
+console.log("Final del script")
