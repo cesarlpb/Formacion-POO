83af00fe861740e393c926e9c4fdecf1fe6a111a
César <cesarlpb@gmail.com>
2022-10-19 15:37:01 +0200

    Ejercicio 2 - prompt() suma

--- 1-Modulo/2-IntroWeb-pt2-(17-21Oct)/19-oct-tarde/script.js
+++ 1-Modulo/2-IntroWeb-pt2-(17-21Oct)/19-oct-tarde/script.js
@@ -1,2 +1,21 @@
 console.log("Ejercicio 2")
 console.log("-----------")
+
+/* Suma */ 
+let suma = document.getElementById("suma") // <p id="suma"  class="inline">Suma: </p>
+let a = Number(prompt("Introduce primer operando:"))
+let b = Number(prompt("Introduce segundo operando:"))
+let resultado = a + b
+console.log("Suma:" + resultado)
+suma.innerText += resultado
+
+/* Resta */ 
+let resta = document.getElementById("resta") // <p id="resta" class="inline">Resta: </p>
+
+resultado = a + b
+let prod = document.getElementById("prod") // <p id="prod"  class="inline">Producto: </p>
+
+resultado = a + b
+let divi = document.getElementById("divi") // <p id="divi"  class="inline">División: </p>
+
+resultado = a + b
