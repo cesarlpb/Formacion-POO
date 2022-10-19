63a82becb35702773f48617192255a8542e72a51
César <cesarlpb@gmail.com>
2022-10-19 16:18:41 +0200

    Ejercicio 2 - prompt() todas las operaciones

--- 1-Modulo/2-IntroWeb-pt2-(17-21Oct)/19-oct-tarde/script.js
+++ 1-Modulo/2-IntroWeb-pt2-(17-21Oct)/19-oct-tarde/script.js
@@ -1,21 +1,41 @@
 console.log("Ejercicio 2")
 console.log("-----------")
 
-/* Suma */ 
-let suma = document.getElementById("suma") // <p id="suma"  class="inline">Suma: </p>
 let a = Number(prompt("Introduce primer operando:"))
 let b = Number(prompt("Introduce segundo operando:"))
+
+/* Suma */ 
+let suma = document.getElementById("suma") // <p id="suma"  class="inline">Suma: </p>
 let resultado = a + b
 console.log("Suma:" + resultado)
-suma.innerText += resultado
+suma.innerText += resultado // 'Suma:'      + 36        ---> 'Suma:36'
+//          suma.innerText = suma.innerText + resultado
+
+/* Desglose de los pasos para la suma */
+// let str = suma.innerText // 'Suma:'
+// resultado = a + b        // 36
+// let newStr = str + resultado // 'Suma:36'
+// suma.innerText = newStr  // 'Suma:36'
+/***********************/
 
 /* Resta */ 
 let resta = document.getElementById("resta") // <p id="resta" class="inline">Resta: </p>
+// a = Number(prompt("Introduce primer operando:"))
+// b = Number(prompt("Introduce segundo operando:"))
+resultado = a - b
+console.log("Resta:" + resultado)
+resta.innerText += resultado
 
-resultado = a + b
 let prod = document.getElementById("prod") // <p id="prod"  class="inline">Producto: </p>
+// a = Number(prompt("Introduce primer operando:"))
+// b = Number(prompt("Introduce segundo operando:"))
+resultado = a * b
+console.log("Prod:" + resultado)
+prod.innerText += resultado
 
-resultado = a + b
 let divi = document.getElementById("divi") // <p id="divi"  class="inline">División: </p>
-
-resultado = a + b
+// a = Number(prompt("Introduce primer operando:"))
+// b = Number(prompt("Introduce segundo operando:"))
+resultado = a / b
+console.log("División:" + resultado)
+divi.innerText += resultado
