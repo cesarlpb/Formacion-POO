11a73c4daa41dd48957fb6d1015cd864f1989016
César <cesarlpb@gmail.com>
2022-10-20 10:20:39 +0200

    Ejercicio 1 - codigo inicial

---
+++ 1-Modulo/2-IntroWeb-pt2-(17-21Oct)/20-oct-mañana/script.js
@@ -0,0 +1,12 @@
+// Ejercicio 1 - 20 oct
+
+/* 
+
+- Pedir un dato al usuario y comprobar si es mayor de edad
+- Comprobar qué día es y sacar un mensaje personalizado en cada caso
+- Pedir dos datos para realizar una división -> comprobar que el divisor (segundo número en a / b sería la b) no es cero 
+
+
+a / 0 // Infinity -> no es número
+
+*/
