d7bbad43750452c2d5d4dd097a7ac700a55c5ae3
César <cesarlpb@gmail.com>
2022-10-25 11:09:32 +0200

    Operadores texto y HTML

--- 1-Modulo/3-IntroWeb-pt3-(24-28Oct)/25-oct/script.js
+++ 1-Modulo/3-IntroWeb-pt3-(24-28Oct)/25-oct/script.js
@@ -6,6 +6,28 @@ let operadores = document.getElementById("operadores");
 let asignacion = document.getElementById("asignacion");
 let tipos = document.getElementById("tipos");
 
-operadores.innerText = "operadores"
+/* Operadores:
++ - * / 
+
+%
+**
+
+++ --
+
+*/
+
+let opCombTexto = "Operación Combinada \n 10 +  22 / 2  + ( 5*7 ) + 2**3 - 20" 
+let opComb = 10 +  22 / 2  + ( 5*7 ) + 2**3 - 20
+
+// para salto de línea cambiar ": " por "\n"
+
+// Alternativa con HTML: 
+
+let operadoresHtml = document.getElementById("operadoresHtml")
+operadoresHtml.innerHTML = "<p style='border:1px solid black; padding: 10px;'>" + opCombTexto + ": " + String(opComb) + "</p>"
+
+operadores.innerText = opCombTexto + ": " + String(opComb)
+
 asignacion.innerText = "asignacion"
+
 tipos.innerText = "tipos"
