d0c8981515063dfbe75a24eced7511ce66b6c739
César <cesarlpb@gmail.com>
2022-10-25 10:40:21 +0200

    Añado script.js
    Setup del script con variables para modificar el contenido de las secciones

---
+++ 1-Modulo/3-IntroWeb-pt3-(24-28Oct)/25-oct/script.js
@@ -0,0 +1,11 @@
+/* Programa para probar ejemplos de operadores (aritméticos), 
+asignación (aritméticos) y tipos de datos en JS 
+*/ 
+
+let operadores = document.getElementById("operadores");
+let asignacion = document.getElementById("asignacion");
+let tipos = document.getElementById("tipos");
+
+operadores.innerText = "operadores"
+asignacion.innerText = "asignacion"
+tipos.innerText = "tipos"
