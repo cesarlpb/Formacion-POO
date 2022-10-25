07f4e54945a9a0a89e71d4fd4ccea30e53d12a34
César <cesarlpb@gmail.com>
2022-10-25 15:04:15 +0200

    Explicación ámbito o scope
    Hemos creado una nueva función que formatea la salida del precio

--- 1-Modulo/3-IntroWeb-pt3-(24-28Oct)/25-oct-tarde/script.js
+++ 1-Modulo/3-IntroWeb-pt3-(24-28Oct)/25-oct-tarde/script.js
@@ -2,19 +2,36 @@
 
 // Ejemplo: función que calcula el precio final de un producto añadiendo 21% de IVA
 
+let variableGlobal = "Texto en línea 5";
+let variableGlobal2 = "Texto en línea 6"
+
 // Definimos variables con el objeto donde vamos a colocar el resultado
 let ejemplo = document.getElementById("ejemplo");
+let ejemplo2 = document.getElementById("ejemplo2");
 // Usar o LLAMAR la función calcularPrecioFinal
-let precio = calcularPrecioFinal(100);
+let precio = calcularPrecioFinal(100); 
 // Escribir el dato recibido de la fn en el elemento ejemplo
 ejemplo.innerText = "El precio calculado es: " + String(precio) + " €.";
+ejemplo2.innerText = calcularPrecioFinal2(200)
 
 /* Ejercicios */
 let ejercicio1 = document.getElementById("ejercicio-1");
-let array = calcularDesglose(200)
+let array = calcularDesglose(100)
 ejercicio1.innerText = array;
 
+// Escribimos variable global
+// Variable Global 
+modificarVariableGlobal()
+console.log(variableGlobal)
+// Variable Global 2
+modificarVariableGlobal2()
+console.log(variableGlobal2)
+
 /* Funciones */
+
+// Input o entrada: precioBase
+// Función que calcula el 21% del IVA y lo suma al precioBase. 
+// Output o salida: precioFinal
 function calcularPrecioFinal(precioBase){
     // Calculamos IVA como el 21% del parámetro precioBase
     let iva = 21/100 * precioBase;
@@ -22,6 +39,26 @@ function calcularPrecioFinal(precioBase){
     let precioFinal = precioBase + iva;
     // Retornamos o devolvemos el precioFinal calculado
     return precioFinal;
+}
+
+/* Función para practicar scope o ámbito */
+function modificarVariableGlobal(){
+    variableGlobal = "Nuevo texto desde función en línea 39"
+}
+/* Función para practicar scope o ámbito */
+function modificarVariableGlobal2(){
+    let variableGlobal2;
+    variableGlobal2 = "Nuevo texto desde función en línea 45"
+}
+
+function calcularPrecioFinal2(precioBase){
+    // Calculamos IVA como el 21% del parámetro precioBase
+    let iva = 21/100 * precioBase;
+    // Calculamos el precioFinal como la suma de precioBase más IVA
+    let precioFinal = precioBase + iva;
+    // Retornamos o devolvemos el precioFinal calculado
+    let salidaFormateada = "El precio calculado es: " + String(precioFinal) + " €.";
+    return salidaFormateada;
 }
 
 // Ejercicio -> recibimos cantidad total y calculamos el desglose de precio base e IVA
