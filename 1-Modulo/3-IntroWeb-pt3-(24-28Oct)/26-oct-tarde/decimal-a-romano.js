356e3bc4982a418119d8a6761e5d7aa0030da9b0
César <cesarlpb@gmail.com>
2022-10-26 16:24:43 +0200

    fn de equivalencias decimal a romano 'simboloRomano()'
    Planteamos la descomposición decimal de un número en sus dígitos

--- 1-Modulo/3-IntroWeb-pt3-(24-28Oct)/26-oct-tarde/decimal-a-romano.js
+++ 1-Modulo/3-IntroWeb-pt3-(24-28Oct)/26-oct-tarde/decimal-a-romano.js
@@ -1,22 +1,11 @@
 // Script para convertir números decimales a romanos
 
-let numRomano = convertir()
-document.getElementById("resultados").innerText = numRomano;
-/*
-1       ->  I
-5       ->  V
-10      ->  X
-50      ->  L
-100     ->  C
-500     ->  D
-1000    ->  M
-*/ 
-
 function convertir(){
     // Leemos el campo del input y llamamos a convertir()
     let numDecimal = document.getElementById("myInputA").valueAsNumber; 
+
     // Validamos que num esté entre 1 y 3999
-    console.log(numDecimal)
+        // Para evitar que salga NaN ponemos un valor por defecto para el input
     if( numDecimal < 1 ){
         alert("Debes ingresar un número positivo")
         return NaN;
@@ -27,10 +16,49 @@ function convertir(){
     
     // Recibe número decimal entero
     
-    // Descomposición del número en sumandos -> 120 -> 100 + 20
-    
     // Equivalencia con símbolos en numeración romana
 
     // Ordenación y eliminación de símbolos no necesarios
-    return numDecimal;
+
+    document.getElementById("resultados").innerText = numDecimal;
+}
+
+function simboloRomano(num) {
+    /* 
+    1       ->  I 
+    5       ->  V
+    10      ->  X
+    50      ->  L
+    100     ->  C
+    500     ->  D
+    1000    ->  M
+    */
+    if( num === 1 ) { return "I" } 
+    else if ( num === 5 )   { return "V"; } 
+    else if ( num === 10 )  { return "X"; } 
+    else if ( num === 50 )  { return "L"; }
+    else if ( num === 100 ) { return "C"; }
+    else if ( num === 500 ) { return "D"; }
+    else if ( num === 1000 ){ return "M"; }
+    else { return NaN; }
+}
+
+// Descomposición del número en sumandos -> 120 -> 100 + 20 -> 1*100 + 2*10
+    // Calcula los sumandos de un número determinado -> 1, 10, 100, 1000
+    
+    /*
+    Ejemplos:
+
+    2022 -> 2*1000 + 2*10 + 2*1 -> [M, C, X, I] -> [2, 0, 2, 2]
+    1989 -> [1, 9, 8, 9] 
+    147  -> [0, 1, 4, 7]
+    65  ->  [0, 0, 6, 5]
+    8   ->  [0, 0, 0, 8]
+    */
+
+function descomponer(num){
+    //         M  C  D  U
+    let arr = [0, 0, 0, 0];
+    // algoritmo
+    return arr;
 }
