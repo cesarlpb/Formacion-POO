2d756a4159d46bfd2d4e3d51cbc698cd155083ab
César <cesarlpb@gmail.com>
2022-10-26 09:21:54 +0200

    Calculadora con botones - código inicial

--- 1-Modulo/3-IntroWeb-pt3-(24-28Oct)/26-oct-mañana/calculadora.js
+++ 1-Modulo/3-IntroWeb-pt3-(24-28Oct)/26-oct-mañana/calculadora.js
@@ -1,6 +1,23 @@
 /* Calculadora */ 
 
+function calc(){
+    let numA = document.getElementById("myInputA")
+    let numB = document.getElementById("myInputB")
+    console.log("Hiciste clic")
+    console.log(numA)
+    console.log(numB)
+}
 // Funciones para operaciones básicas + - * / ** % sqrt() log() 
+// Suma
+function suma(a, b){
+    return a + b;
+}
+// Resta
+// Producto
+// División
+// Raíz cuadrada -> Math.sqrt(num)
+// Potencia -> Math.pow(base, exponente)
+// Log -> Math.log(num)
 // Botones para realizar cálculos
 // inputs para ingresar datos
 // Estilos
