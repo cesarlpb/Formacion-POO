60b8b1800c60cfa0b2c2960e573104a32f367925
César <cesarlpb@gmail.com>
2022-10-27 12:36:58 +0200

    Implementamos el <select> para elegir número de decimales de la salida

--- 1-Modulo/3-IntroWeb-pt3-(24-28Oct)/27-oct/fraccion-a-decimal.js
+++ 1-Modulo/3-IntroWeb-pt3-(24-28Oct)/27-oct/fraccion-a-decimal.js
@@ -3,6 +3,15 @@
 function convertir(){
     let numerador = document.getElementById("myInputA").valueAsNumber;
     let denominador = document.getElementById("myInputB").valueAsNumber;
+
+    /* Excluimos el caso de dividir por cero */
+    if( denominador === 0 ){ 
+        alert("No se puede dividir por cero.\n¡Acaba mal!" )
+        return ""
+    }
+
+    let decimales = document.getElementById("decimales").options.selectedIndex;
     let resultado = numerador / denominador
-    document.getElementById("resultados").innerText = String(resultado)
+    
+    document.getElementById("resultados").innerText = String(resultado.toFixed(decimales))
 }
