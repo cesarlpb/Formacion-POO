93eaa6853bce32544551afec7750bc4daf0b43e1
César <cesarlpb@gmail.com>
2022-10-28 08:33:47 +0200

    Ejercicios bucle for - código inicial

---
+++ 1-Modulo/3-IntroWeb-pt3-(24-28Oct)/28-oct-mañana/bucle-for-ej.js
@@ -0,0 +1,33 @@
+/* Script para resolver ejercicios de bucle for con JS */
+
+// 1
+/*
+Usando un bucle for escribir un script que pida un valor entero y 
+muestre en pantalla una lista de números desde el 0 al valor tecleado. 
+Los números se separarán por comas. 
+
+Salida:
+0, 1, 2, 3, 4, 5  
+*/
+
+// 2 
+/*
+Se trata de que dado un número entero el script muesre una cuenta atrás, 
+es decir, una lista de numeros separados por coma desde el número tecleado hasta el 0.
+
+Salida:
+5,4,3,2,1
+*/
+
+// 3 
+/*
+Se quiere un bucle que genere una lista con todos los números pares positivos por debajo 
+del número tecleado por el usuario. Los números se aparecerán en una ventana alert con 
+cambios de linea (carácter '\n'). 
+
+Salida
+2
+4
+6
+...
+*/
