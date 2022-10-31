59a43404bd46dcf39bffd7b55f4987ecc4b8cedc
César <cesarlpb@gmail.com>
2022-10-31 10:38:06 +0100

    Ej 2. Calcular precio neto de producto

--- 1-Modulo/4-POO(31Oct-4Nov)/31-oct-mañana/ej-objetos.js
+++ 1-Modulo/4-POO(31Oct-4Nov)/31-oct-mañana/ej-objetos.js
@@ -1,6 +1,7 @@
 /* Ejercicios con Objetos en JS */
 
-ej1();
+//ej1(); // Obj alumno y método que promedia notas
+ej2();
 
 // Ej 1 - Obj para guardar notas de alumno y método que promedia notas
     // Propiedades: nombre, apellido, ingles, prog, html (asignaturas)
@@ -18,12 +19,26 @@ function ej1(){
             let prog = this.prog;
             let html = this.html;
             let res = (ingles + prog + html) / 3 
-            return res.toFixed(2); 
+            return res.toFixed(2);
+
+            // Alternativa del return sin variables adicionales:
+            //return ( (this.ingles + this.prog + this.html) / 3 ).toFixed(2); 
         }
     }
     escribirResultado("Promedio de notas", alumno.promediarNotas())
 }
 
+function ej2(){
+    let producto = {
+        "precio": 200,      // €
+        "descuento": 30,    // Esto es un % -> tanto por ciento
+        "calcularPrecioNeto": function(){
+            let valorDescontado = (this.descuento / 100) * this.precio; // €
+            return this.precio - valorDescontado;
+        }
+    }
+    escribirResultado("Calculamos precio neto", producto.calcularPrecioNeto() + " €")
+}
 
 // Funciones auxiliares
 function escribirResultado(op, res){
@@ -31,7 +46,7 @@ function escribirResultado(op, res){
     let descripcion = "";
     let resultado = "";
     // Lo que hemos calculado
-    descripcion += "Operación realizada: " + op + "\n";
+    descripcion += "Operación realizada: " + op + "\n\n";
     // Resultado
     resultado += "Resultado:\n\n" + res;
     // Escribimos en el div
