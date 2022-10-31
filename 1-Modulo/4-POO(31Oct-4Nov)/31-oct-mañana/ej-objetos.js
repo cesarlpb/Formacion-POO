1d2e71db20faeaa9d581b5f0d424c24389df1d5d
César <cesarlpb@gmail.com>
2022-10-31 11:17:20 +0100

    Ej 3. Objeto piedra y densidad

--- 1-Modulo/4-POO(31Oct-4Nov)/31-oct-mañana/ej-objetos.js
+++ 1-Modulo/4-POO(31Oct-4Nov)/31-oct-mañana/ej-objetos.js
@@ -1,12 +1,12 @@
 /* Ejercicios con Objetos en JS */
 
 //ej1(); // Obj alumno y método que promedia notas
-ej2();
+//ej2(); // Obj para calcular el precio neto de un producto
+ej3();
 
 // Ej 1 - Obj para guardar notas de alumno y método que promedia notas
     // Propiedades: nombre, apellido, ingles, prog, html (asignaturas)
     // Método: calcular media de las tres asignaturas
-
 function ej1(){
     let alumno = {
         "nombre": "Pepe",
@@ -28,6 +28,7 @@ function ej1(){
     escribirResultado("Promedio de notas", alumno.promediarNotas())
 }
 
+// Ej 2 - Cálculo del precio neto de un producto aplicando descuento
 function ej2(){
     let producto = {
         "precio": 200,      // €
@@ -38,6 +39,29 @@ function ej2(){
         }
     }
     escribirResultado("Calculamos precio neto", producto.calcularPrecioNeto() + " €")
+}
+
+// Ej 3 - Objeto piedra que tiene masa y volumen a partir de lo cual de calcula densidad
+function ej3(){
+    let piedra = {
+        "color": "grisáceo",
+        "origen": "Islas volcánicas",
+        "materiales": ["silicio", "calcio"],
+        "masaKg": 1.5,          // kg   
+        "masaGr": 1.5 * 1000,
+        "volumenCc": 100,
+        "volumenM3": 0.001,
+        "calcDensidadKgM3": function(){
+            // densidad = masa (kg) / volumen (m3)
+            return (this.masaKg / this.volumenM3).toFixed(5); // kg/m3
+        },
+        "calcDensidadGrCc": function(){
+            // densidad = masa (gr) / volumen (cc)
+            return (this.masaGr / this.volumenCc).toFixed(5); // gr/cc
+        }
+    }
+    // escribirResultado("Densidad en kg/m3:", piedra.calcDensidadKgM3() + " kg/m3");
+    escribirResultado("Densidad en gr/cc:", piedra.calcDensidadGrCc() + " g/cc")
 }
 
 // Funciones auxiliares
