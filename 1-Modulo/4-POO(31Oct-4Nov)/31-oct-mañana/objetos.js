a2f5012e9054ffa679610abbb140dfae15dff577
César <cesarlpb@gmail.com>
2022-10-31 09:23:25 +0100

    Ejercicios Objetos código inicial

--- 1-Modulo/4-POO(31Oct-4Nov)/31-oct-mañana/objetos.js
+++ 1-Modulo/4-POO(31Oct-4Nov)/31-oct-mañana/objetos.js
@@ -1,1 +1,23 @@
 /* Script para practicar los objetos en JS */
+
+/* Resumen de Objetos */
+
+let obj = {
+    "atributo": "Valor del atributo", 
+    "nombreDelMetodo": function(){alert("Mensaje desde método")},
+    "otraPropiedad": "Valor de la otra Propiedad",
+    "escribirAtributo": function(){ console.log("atributo: " + this.atributo) },
+    "escribirOtraPropiedad": function(){ console.log("otraPropiedad: " + this.otraPropiedad) }
+}
+
+/* Calculadora con + - * / */
+
+let calc = {
+    "operaciones": "Suma, Resta, Producto, Division",
+    "operadores": "+ - * /",
+    "suma": function(a,b){ console.log( a + b ) },
+    "resta": function(a,b){ console.log( a - b) },
+    "prod": function(a,b){ console.log( a * b ) },
+    "divi": function(a,b){ console.log( a / b) }
+}
+/********************/
---
+++ 1-Modulo/4-POO(31Oct-4Nov)/ej-objetos.js
@@ -0,0 +1,1 @@
+/* Ejercicios con Objetos en JS */
