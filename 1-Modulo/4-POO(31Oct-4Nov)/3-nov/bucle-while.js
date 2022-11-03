81d032e52c7bd34cfd52ee42b854846bad82eef0
César <cesarlpb@gmail.com>
2022-11-03 09:38:38 +0100

    Ej 1 resuelto

--- 1-Modulo/4-POO(31Oct-4Nov)/3-nov/bucle-while.js
+++ 1-Modulo/4-POO(31Oct-4Nov)/3-nov/bucle-while.js
@@ -2,7 +2,15 @@
 
 // 1. Imprimir lista de números de 0 o 1 a N (número introducido por usuario)
 function ej1(){
-
+    let el = document.getElementById("ej-1-resultados");
+    let n = document.getElementById("input-1").valueAsNumber;
+    let i = 0;
+    let res = "";
+    while( i < n ){
+        res += i + " ";
+        i++;
+    }
+    el.innerText = res;
 }
 // 2. Se pide número en rango y se sigue pidiendo hasta recibirlo
 
