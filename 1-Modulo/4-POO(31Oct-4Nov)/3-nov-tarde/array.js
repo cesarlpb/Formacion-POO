be10b2b1804bae77a8234147800b7f0ade606e4a
César <cesarlpb@gmail.com>
2022-11-03 14:49:03 +0100

    Ejs 1-5 Enunciados

--- 1-Modulo/4-POO(31Oct-4Nov)/3-nov-tarde/array.js
+++ 1-Modulo/4-POO(31Oct-4Nov)/3-nov-tarde/array.js
@@ -1,1 +1,29 @@
 // Script para resolver ejercicios con Arrays
+
+// 1. Array para guardar dias de la semana y al pedir 0-6 lo escribe
+function ej1(){
+
+}
+// 2. Array con booleans -> "V" o "F" 
+    // si recibimos 0-9 -> true o "V"
+    // else return false o "F"
+function ej2(){
+
+}
+// 3. fn sumaLista(arr) -> return de suma de Numbers
+function ej3(){
+
+}
+// 4. Guardar números en array
+    // Entrada de datos acaba con 0 o char alfanumérico -> ç r /
+    // Se muestra el array, cuántos pares, impares
+    // No guardar cosas que no sean Number
+function ej4(){
+
+}
+// 5. Devolvemos min o índice del mínimo controlando "v" o "i" como segundo param
+    // min([4,5,7], 'v') -> 4
+    // min([4,5,7], 'i') -> 0
+function ej5(){
+
+}
