492768907ef642607c608f1bb36fe50d26b0d7ed
César <cesarlpb@gmail.com>
2022-11-03 09:26:43 +0100

    Enunciados

---
+++ 1-Modulo/4-POO(31Oct-4Nov)/3-nov/bucle-while.js
@@ -0,0 +1,15 @@
+// Script para hacer ejercicios de bucle While
+
+// 1. Imprimir lista de números de 0 o 1 a N (número introducido por usuario)
+
+// 2. Se pide número en rango y se sigue pidiendo hasta recibirlo
+
+// 3. Se pregunta si continuamos programa -> n o N cierran bucle
+
+// 4. Determinar si el número introducido por usuario es primo
+    // Número primo = divisible por si mismo y 1
+
+// 5. Pide una contraseña y si:
+    // Coincide -> Acceso concedido
+    // No coincide -> 3 intentos
+    // Después, mensaje de alerta
