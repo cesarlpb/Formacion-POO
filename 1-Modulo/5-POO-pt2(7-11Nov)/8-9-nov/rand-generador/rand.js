37d30d5a3912761b81a87075e90c49179fb114ff
César <cesarlpb@gmail.com>
2022-11-09 17:01:22 +0100

    Función generarRands()

--- 1-Modulo/5-POO-pt2(7-11Nov)/8-9-nov/rand-generador/rand.js
+++ 1-Modulo/5-POO-pt2(7-11Nov)/8-9-nov/rand-generador/rand.js
@@ -2,3 +2,13 @@
 *  - Escribimos resultado en consola
 *  - Aceptamos param para calcular N preguntas
 */
+
+function generarRands(n){
+    let arr = [];
+    for(let i = 1; i <= n; i++){
+        let num = Math.random()*53
+        num = Math.round(num)
+        arr.push(num)
+    }
+    return arr;
+}
