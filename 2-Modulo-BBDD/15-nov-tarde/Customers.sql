080975682b2c4f3e13db2be99639b2b36ec228e7
César <cesarlpb@gmail.com>
2022-11-15 16:07:25 +0100

    Creación de tabla Customers y datos iniciales

---
+++ 2-Modulo-BBDD/15-nov-tarde/Customers.sql
@@ -0,0 +1,9 @@
+CREATE TABLE `w3schools-sql`.`Customers` (
+  `CustomerID` INT NOT NULL AUTO_INCREMENT,
+  `CustomerName` VARCHAR(50) NOT NULL,
+  `ContactName` VARCHAR(100) NOT NULL,
+  `Address` VARCHAR(200) NOT NULL,
+  `City` VARCHAR(50) NOT NULL,
+  `PostalCode` VARCHAR(50) NOT NULL,
+  `Country` VARCHAR(50) NOT NULL,
+  PRIMARY KEY (`CustomerID`));
---
+++ 2-Modulo-BBDD/15-nov-tarde/SeedCustomers.sql
@@ -0,0 +1,5 @@
+INSERT INTO `w3schools-sql`.`Customers` (`CustomerName`, `ContactName`, `Address`, `City`, `PostalCode`, `Country`) VALUES ('María', 'Juanito Pérez', 'Calle 123 s/n ', 'Madrid', '28001', 'España');
+INSERT INTO `w3schools-sql`.`Customers` (`CustomerName`, `ContactName`, `Address`, `City`, `PostalCode`, `Country`) VALUES ('Pepito', 'Mario Martínez', 'Av. de la Programación 098', 'Buenos Aires', '6660', 'Argentina');
+INSERT INTO `w3schools-sql`.`Customers` (`CustomerName`, `ContactName`, `Address`, `City`, `PostalCode`, `Country`) VALUES ('Alfonso', 'María Santiago', 'C/ De La Fuente 345', 'Toledo', '45660', 'España');
+INSERT INTO `w3schools-sql`.`Customers` (`CustomerName`, `ContactName`, `Address`, `City`, `PostalCode`, `Country`) VALUES ('Mark', 'John Smith', '1253 Crummit Lane', 'Omaha', '68137', 'Estados Unidos');
+INSERT INTO `w3schools-sql`.`Customers` (`CustomerName`, `ContactName`, `Address`, `City`, `PostalCode`, `Country`) VALUES ('Andrea', 'Jose Cuervo', 'CIRC JUAN PABLO II NO. 1755 Int. NO. 5', 'Puebla', '72410', 'México');
