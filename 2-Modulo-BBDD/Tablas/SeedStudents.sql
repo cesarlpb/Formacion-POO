75133a28e4b9289f8a977c19385abeb36b47a1ee
César <cesarlpb@gmail.com>
2022-11-16 15:42:08 +0100

    Update en tabla Students

--- 2-Modulo-BBDD/Tablas/SeedStudents.sql
+++ 2-Modulo-BBDD/Tablas/SeedStudents.sql
@@ -1,7 +1,10 @@
 -- Datos para tabla Students
 
-INSERT INTO `w3schools-sql`.`Students` (`Nombre`, `Examen`) VALUES ('María', '7');
+INSERT INTO `w3schools-sql`.`Students` (`Nombre`, `Examen`) VALUES ('Mari', '7');
 INSERT INTO `w3schools-sql`.`Students` (`Nombre`, `Examen`) VALUES ('Juan', '5');
 INSERT INTO `w3schools-sql`.`Students` (`Nombre`, `Examen`) VALUES ('Alfonso', '8');
 INSERT INTO `w3schools-sql`.`Students` (`Nombre`, `Examen`) VALUES ('Mario', '5');
 INSERT INTO `w3schools-sql`.`Students` (`Nombre`, `Examen`) VALUES ('Ana', '6');
+
+-- UPDATE
+UPDATE `w3schools-sql`.`Students` SET `Nombre` = 'Mari' WHERE (`StudentsId` = '1');
