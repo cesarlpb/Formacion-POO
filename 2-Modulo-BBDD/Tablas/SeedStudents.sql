ffe87ac96630a10941895d01829afa04be072898
César <cesarlpb@gmail.com>
2022-11-16 17:00:46 +0100

    Editamos datos de tablas

--- 2-Modulo-BBDD/Tablas/SeedStudents.sql
+++ 2-Modulo-BBDD/Tablas/SeedStudents.sql
@@ -8,3 +8,9 @@ INSERT INTO `w3schools-sql`.`Students` (`Nombre`, `Examen`) VALUES ('Ana', '6');
 
 -- UPDATE
 UPDATE `w3schools-sql`.`Students` SET `Nombre` = 'Mari' WHERE (`StudentsId` = '1');
+
+-- Añadimos fechas de los exámenes
+UPDATE `w3schools-sql`.`Students` SET `Fecha` = '02-11-2022' WHERE (`StudentsId` = '2');
+UPDATE `w3schools-sql`.`Students` SET `Fecha` = '05-11-2022' WHERE (`StudentsId` = '3');
+UPDATE `w3schools-sql`.`Students` SET `Fecha` = '16-11-2022' WHERE (`StudentsId` = '4');
+UPDATE `w3schools-sql`.`Students` SET `Fecha` = '11-11-2022' WHERE (`StudentsId` = '5');
--- 2-Modulo-BBDD/Tablas/Students.sql
+++ 2-Modulo-BBDD/Tablas/Students.sql
@@ -5,3 +5,6 @@ CREATE TABLE `w3schools-sql`.`Students` (
   `Nombre` VARCHAR(50) NULL DEFAULT 'Alumno',
   `Examen` DECIMAL(3,2) NULL DEFAULT 1,
   PRIMARY KEY (`StudentsId`));
+
+ALTER TABLE `w3schools-sql`.`Students` 
+ADD COLUMN `Fecha` VARCHAR(50) NOT NULL DEFAULT '01-11-2022' COMMENT 'Usamos formato de dd-mm-aaaa' AFTER `Examen`;
