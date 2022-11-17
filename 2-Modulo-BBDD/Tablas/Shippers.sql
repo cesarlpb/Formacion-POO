63114547d4a155023388885873f3b61fcbb4dd64
César <cesarlpb@gmail.com>
2022-11-17 11:46:20 +0100

    Edit en Shippers -> ShipperId renombrado

--- 2-Modulo-BBDD/Tablas/Shippers.sql
+++ 2-Modulo-BBDD/Tablas/Shippers.sql
@@ -1,5 +1,5 @@
 CREATE TABLE `w3schools-sql`.`Shippers` (
-  `ShippersId` INT NOT NULL AUTO_INCREMENT,
+  `ShipperId` INT NOT NULL AUTO_INCREMENT,
   `ShipperName` VARCHAR(100) NOT NULL,
   `Phone` VARCHAR(20) NULL DEFAULT '(+1) 123-456-789' COMMENT 'Tabla de Empresas de Entrega de paquetes',
   PRIMARY KEY (`ShippersId`));
