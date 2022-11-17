639a5c0f75292d6e1c20284c2b214a1446f0038e
César <cesarlpb@gmail.com>
2022-11-17 11:09:34 +0100

    Creamos OrderDetails

---
+++ 2-Modulo-BBDD/Seeds/SeedOrderDetails.sql
@@ -0,0 +1,5 @@
+INSERT INTO `w3schools-sql`.`OrderDetails` (`OrderId`, `ProductId`, `Quantity`) VALUES ('1003', '3', '3');
+INSERT INTO `w3schools-sql`.`OrderDetails` (`OrderId`, `ProductId`, `Quantity`) VALUES ('1004', '5', '2');
+INSERT INTO `w3schools-sql`.`OrderDetails` (`OrderId`, `ProductId`) VALUES ('1002', '8');
+INSERT INTO `w3schools-sql`.`OrderDetails` (`OrderId`, `ProductId`, `Quantity`) VALUES ('1000', '9', '4');
+INSERT INTO `w3schools-sql`.`OrderDetails` (`OrderId`, `ProductId`) VALUES ('1001', '2');
---
+++ 2-Modulo-BBDD/Tablas/OrderDetails.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `w3schools-sql`.`OrderDetails` (
+  `OrderDetailId` INT NOT NULL AUTO_INCREMENT,
+  `OrderId` INT NOT NULL,
+  `ProductId` INT NOT NULL,
+  `Quantity` INT NULL DEFAULT 1 COMMENT 'Unidades del producto (int)',
+  PRIMARY KEY (`OrderDetailId`));
