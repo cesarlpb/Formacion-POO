27372f7eee33f94b5d41240eada074daf78e9b2d
César <cesarlpb@gmail.com>
2022-11-17 11:35:47 +0100

    Creamos tabla Suppliers

---
+++ 2-Modulo-BBDD/Tablas/Suppliers.sql
@@ -0,0 +1,10 @@
+CREATE TABLE `w3schools-sql`.`Suppliers` (
+  `SupplierId` INT NOT NULL AUTO_INCREMENT,
+  `SupplierName` VARCHAR(100) NOT NULL DEFAULT 'Supplier',
+  `ContactName` VARCHAR(100) NOT NULL DEFAULT 'Persona de Contacto',
+  `Address` VARCHAR(100) NOT NULL DEFAULT 'Default address',
+  `City` VARCHAR(30) NOT NULL DEFAULT 'Default city',
+  `PostalCode` VARCHAR(20) NOT NULL DEFAULT '00001',
+  `Country` VARCHAR(30) NOT NULL DEFAULT 'Default country',
+  `Phone` VARCHAR(20) NOT NULL DEFAULT '+1 123 456 789',
+  PRIMARY KEY (`SupplierId`));
