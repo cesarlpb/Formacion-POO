87b56183a548b1254b8e89bb2208d9e37e174956
César <cesarlpb@gmail.com>
2022-11-17 10:13:27 +0100

    Editamos tabla Orders

--- 2-Modulo-BBDD/Tablas/Orders.sql
+++ 2-Modulo-BBDD/Tablas/Orders.sql
@@ -3,3 +3,8 @@ CREATE TABLE `w3schools-sql`.`Orders` (
   `CustomerId` INT NOT NULL,
   `OrderDate` VARCHAR(10) NULL DEFAULT '01-11-2022',
   PRIMARY KEY (`OrderId`));
+
+  -- UPDATE
+  ALTER TABLE `w3schools-sql`.`Orders` 
+ADD COLUMN `EmployeeId` INT NOT NULL AFTER `OrderDate`,
+ADD COLUMN `ShipperId` INT NOT NULL AFTER `EmployeeId`;
--- 2-Modulo-BBDD/Tablas/SeedOrders.sql
+++ 2-Modulo-BBDD/Tablas/SeedOrders.sql
@@ -2,3 +2,13 @@ INSERT INTO `w3schools-sql`.`Orders` (`OrderId`, `CustomerId`, `OrderDate`) VALU
 INSERT INTO `w3schools-sql`.`Orders` (`CustomerId`, `OrderDate`) VALUES ('4', '05-11-2022');
 INSERT INTO `w3schools-sql`.`Orders` (`CustomerId`, `OrderDate`) VALUES ('7', '10-11-2022');
 INSERT INTO `w3schools-sql`.`Orders` (`CustomerId`, `OrderDate`) VALUES ('5', '03-11-2022');
+
+-- UPDATE
+INSERT INTO `w3schools-sql`.`Orders` (`CustomerId`) VALUES ('11');
+
+-- Después de ALTER TABLE
+UPDATE `w3schools-sql`.`Orders` SET `EmployeeId` = '10', `ShipperId` = '123' WHERE (`OrderId` = '1000');
+UPDATE `w3schools-sql`.`Orders` SET `EmployeeId` = '40', `ShipperId` = '345' WHERE (`OrderId` = '1001');
+UPDATE `w3schools-sql`.`Orders` SET `EmployeeId` = '55', `ShipperId` = '234' WHERE (`OrderId` = '1002');
+UPDATE `w3schools-sql`.`Orders` SET `EmployeeId` = '33', `ShipperId` = '109' WHERE (`OrderId` = '1003');
+UPDATE `w3schools-sql`.`Orders` SET `EmployeeId` = '76', `ShipperId` = '451' WHERE (`OrderId` = '1004');
