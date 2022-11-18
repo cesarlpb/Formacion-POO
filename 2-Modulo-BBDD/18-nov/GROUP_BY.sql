df71d14a89f1be66cf2ccb403ec7982eb8e1498b
César <cesarlpb@gmail.com>
2022-11-18 15:47:22 +0100

    GROUP BY

---
+++ 2-Modulo-BBDD/18-nov/GROUP_BY.sql
@@ -0,0 +1,15 @@
+-- Contamos cuántos usuarios hay de cada país:
+SELECT COUNT(CustomerID), Country
+FROM `w3schools-sql`.Customers
+GROUP BY Country;
+
+-- Agrupamos por cuenta de países y ordenamos asc:
+SELECT COUNT(CustomerID), Country
+FROM `w3schools-sql`.Customers
+GROUP BY Country
+ORDER BY COUNT(CustomerID) ASC;
+
+-- Agrupamos Shippers por número de pedidos que tienen asignados:
+SELECT Shippers.ShipperName,COUNT(Orders.OrderID) AS NumberOfOrders FROM Orders
+LEFT JOIN Shippers ON Orders.ShipperID = Shippers.ShipperID
+GROUP BY ShipperName;
