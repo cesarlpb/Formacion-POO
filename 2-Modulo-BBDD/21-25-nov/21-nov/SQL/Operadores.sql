ed1e97fc710f0fba1ab27b2b4573dfaeb4520ff5
César <cesarlpb@gmail.com>
2022-11-21 10:59:12 +0100

    Ejercicios de Operadores (por hacer)

---
+++ 2-Modulo-BBDD/21-25-nov/21-nov/SQL/Operadores.sql
@@ -0,0 +1,8 @@
+-- Cheatsheet de Operadores
+
+-- Completar ejercicios de todos los operadores de https://www.w3schools.com/mysql/mysql_operators.asp
+-- Aritméticos
+-- Bitwise
+-- Comparación
+-- Compuestos - check
+-- Lógicos
