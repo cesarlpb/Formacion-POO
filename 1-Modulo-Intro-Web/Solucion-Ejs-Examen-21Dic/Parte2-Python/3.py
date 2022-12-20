acfd3c7aaa97e2a31c80b4079dd2e6bab48dde71
César <cesarlpb@gmail.com>
2022-12-20 10:18:34 +0100

    Ejercicio 3 - código inicial

---
+++ 1-Modulo-Intro-Web/Solucion-Ejs-Examen-21Dic/Parte2-Python/3.py
@@ -0,0 +1,17 @@
+# Define las siguientes funciones con un nombre explicativo sobre las acciones que realiza cada una:
+
+#%% a. Se necesita una función que reciba un número cualquiera y retorne un float con dos decimales añadiendo el 10% al número recibido. Si recibe 10.0, debe retornar 11.00.
+
+#%%  b. Escribe una función que reciba dos números cualesquiera y retorne un tablero 3x3 de tres en raya donde el primer número sea la cantidad de X y el segundo la cantidad de O colocados en cualquier orden.
+# Si recibes 4 y 5 debes retornar:
+#   X O O
+#   X O X
+#   X O O
+
+
+
+#%% c. Cuenta el número de veces que aparece el caracter $ en este texto:
+
+texto = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore $ et dolore magna aliqua. Ut enim ad minim $ veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt $ mollit anim id est laborum."
+
+# Debe devolver 3.
