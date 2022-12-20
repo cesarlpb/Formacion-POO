ed85bf787236b1e4923810771fb093b4cd8cada2
César <cesarlpb@gmail.com>
2022-12-20 16:59:45 +0100

    Ej 5 - Contar hasta 100 - código inicial

---
+++ 1-Modulo-Intro-Web/Solucion-Ejs-Examen-21Dic/Parte3-Algoritmos/5.py
@@ -0,0 +1,6 @@
+# Contar del 1 al 100 - Challenge 24
+
+# Enunciado: Quiero contar del 1 al 100 de uno en uno (imprimiendo cada uno). ¿De cuántas maneras eres capaz de hacerlo? Crea el código para cada una de ellas.Escribe tres formas de realizar la cuenta.
+
+# ¿Qué complejidad tiene en tiempo y en espacio cada una de estas formas?
+# ¿Cuánto crees que podría tardar el método más eficiente en procesar 1 millón de datos? Explica en qué basas esta suposición o la forma en la que lo has determinado.
