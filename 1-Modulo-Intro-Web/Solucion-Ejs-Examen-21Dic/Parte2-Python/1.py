802213bce3219c6b6574f75789c1fcd6d9bb89c6
César <cesarlpb@gmail.com>
2022-12-20 09:53:18 +0100

    Ejercicio 1 resuelto

--- 1-Modulo-Intro-Web/Solucion-Ejs-Examen-21Dic/Parte2-Python/1.py
+++ 1-Modulo-Intro-Web/Solucion-Ejs-Examen-21Dic/Parte2-Python/1.py
@@ -1,4 +1,32 @@
 # Realizar las siguientes operaciones con el tipo de bucle que te parezca más adecuado:
-#   a. Calcula el factorial de 5, debe devolver 120.
-#   b. Determina si la palabra “banana” es palíndromo.
-#   c. Ordenar la lista siguiente: [3, 75, 26, 93, 34, 74, 62, 22, 24, 63]
+#%%   a. Calcula el factorial de 5, debe devolver 120.
+factorial = 1
+for i in range(1, 6):
+    factorial *= i
+print("5!", factorial)
+
+#%%   b. Determina si la palabra “banana” es palíndromo.
+
+str_input = "banana"
+str_invertido = ""
+n = len(str_input)
+for idx, char in enumerate(str_input):
+    str_invertido += str_input[n - idx - 1]
+print(str_input, str_invertido, str_invertido == str_input)
+
+#%%   c. Ordenar la lista siguiente: [3, 75, 26, 93, 34, 74, 62, 22, 24, 63]
+
+lista_input = [3, 75, 26, 93, 34, 74, 62, 22, 24, 63]
+lista_ordenada = []
+for numero in lista_input:
+    if len(lista_ordenada) == 0:
+        lista_ordenada.append(numero)
+    else:
+        for idx, numero_ordenado in enumerate(lista_ordenada):
+            if numero < numero_ordenado:
+                lista_ordenada.insert(idx, numero)
+                break
+            elif idx == len(lista_ordenada) - 1:
+                lista_ordenada.append(numero)
+                break
+print(lista_ordenada)
