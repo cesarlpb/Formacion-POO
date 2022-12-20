32c7eff931d9d20be03bee56a7b04de4c54820c5
César <cesarlpb@gmail.com>
2022-12-20 16:58:58 +0100

    Ej 4 - factorial - código inicial

---
+++ 1-Modulo-Intro-Web/Solucion-Ejs-Examen-21Dic/Parte3-Algoritmos/4.py
@@ -0,0 +1,11 @@
+# Factorial
+
+ 
+# Enunciado: Escribe una función que calcule y retorne el factorial de un número dado de forma recursiva.
+ 
+# 	- No se consideran números negativos
+# 	- 0! y 1! son 1
+ 
+
+# ¿Qué complejidad tiene en tiempo y en espacio?
+# ¿Cuánto crees que podría tardar en procesar 1 millón de datos? Explica en qué basas esta suposición o la forma en la que lo has determinado.
