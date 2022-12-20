1c731c65b72bce77670a28b66afa84483a4bd269
César <cesarlpb@gmail.com>
2022-12-20 09:56:16 +0100

    Ejercicio 2 - código inicial

---
+++ 1-Modulo-Intro-Web/Solucion-Ejs-Examen-21Dic/Parte2-Python/2.py
@@ -0,0 +1,12 @@
+# Realiza las siguientes operaciones con listas:
+#%%   a. Ordenar de forma descendente: [19, 41, 60, 65, 67, 68, 77, 82, 85, 95]
+
+#%%   b. Crea una lista de 10 números aleatorios que sean enteros del 1 al 100
+
+
+#%%   c. Usa listas para separar el texto siguiente y borrar los espacios que sobran:
+
+"""Este texto	tiene	espacios arbitrarios.
+Y 		varias líneas.
+Además, tiene		espacios delante, en medio			y al final 		de las líneas."""	
+#   El resultado debe ser un texto sin espacios arbitrarios entre palabras y tres líneas. Puedes guardar cada línea en una lista.
