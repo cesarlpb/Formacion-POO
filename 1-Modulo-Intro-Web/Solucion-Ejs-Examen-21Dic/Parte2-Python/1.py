d90475132661781db84d3b0555c343a6fde804bf
César <cesarlpb@gmail.com>
2022-12-20 09:40:10 +0100

    Ejercicio 1 de Python (parte 2) - código inicial

---
+++ 1-Modulo-Intro-Web/Solucion-Ejs-Examen-21Dic/Parte2-Python/1.py
@@ -0,0 +1,4 @@
+# Realizar las siguientes operaciones con el tipo de bucle que te parezca más adecuado:
+#   a. Calcula el factorial de 5, debe devolver 120.
+#   b. Determina si la palabra “banana” es palíndromo.
+#   c. Ordenar la lista siguiente: [3, 75, 26, 93, 34, 74, 62, 22, 24, 63]
